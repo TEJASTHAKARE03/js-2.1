f3f62c21e0ee140d5ee1f824bb9a5f790669dc9f
TEJASTHAKARE03 <121221202+TEJASTHAKARE03@users.noreply.github.com>
2025-01-16 15:47:17 +0000

    datatypes and variables

---
+++ datatype.js
@@ -0,0 +1,10 @@
+// premitives there 7 types of it
+let age=100;//number
+let name="tejas";//string
+let handsome=true;//boolean
+let x;//undefined
+console.log(x);
+let y=null;//null
+let sym=Symbol("id")//symbol
+console.log(sym);
+console.table([age,name,handsome,x,y,sym]);
---
+++ variables_01.js
@@ -0,0 +1,7 @@
+let admin=01;
+let name="tejas";
+let one=admin;
+admin=name;
+name=one;
+console.log(admin);
+console.log(name);
